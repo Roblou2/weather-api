9652a6cf18b21af0a96e3aa80c719ef7c8ef4d40
Rob Hamburg <robhamburg89@gmail.com>
2023-05-14 20:08:20 +0100

    get API data for all necessary weather aspects

---
+++ scrap.js
@@ -0,0 +1,3 @@
+console.log(`${data.location.name}, ${data.location.region}, ${data.location.country}, ${data.current.condition.text}, temp: ${data.current.temp_c} °C, feels like: ${data.current.feelslike_c} °C, windspeed: ${data.current.wind_mph} mph, humidity: ${data.current.humidity} % `)
+
+console.log(forecastResponse)
--- script.js
+++ script.js
@@ -9,12 +9,31 @@ async function loadSearched () {
     let CurrentWeatherInUrl = "https://api.weatherapi.com/v1/current.json?key=f64289458a7443a396c160259230705&q="
     let searchedUrl = CurrentWeatherInUrl.concat(`${searched}`)
 
+        let forecastURL = "https://api.weatherapi.com/v1/forecast.json?key=f64289458a7443a396c160259230705&q="
+    let forecastURLSearched = forecastURL.concat(`${searched}`.concat('&days=5'))
+
+    const response = await fetch(`${searchedUrl}`, {mode: 'cors'})
+    const data = response.json()
+    
+     const forecastResponse = await fetch(`${forecastURLSearched }`, {mode: 'cors'})
+     const forecastResponseData = forecastResponse.json()
+
+     //array of current location weather and the weather forecast
+     const theWeather = Promise.all([data, forecastResponseData]);
     try {
- const response = await fetch(`${searchedUrl}`, {mode: 'cors'})
-const data = await response.json()
-console.log(data.current.condition.text)
- 
-    }
+const all = await theWeather
+
+console.log(`${all[0].location.name}, ${all[0].location.region}, ${all[0].location.country}, ${all[0].current.condition.text}, temp: ${all[0].current.temp_c} °C, feels like: ${all[0].current.feelslike_c} °C, windspeed: ${all[0].current.wind_mph} mph, humidity: ${all[0].current.humidity} % `)
+
+
+for (let d = 0; d < all[1].forecast.forecastday.length; d++) {
+  console.log(all[1].forecast.forecastday[d].day.condition.text)
+}
+    
+  
+}
+
+    
      catch  (error) {
 console.error("Not found")
      }
